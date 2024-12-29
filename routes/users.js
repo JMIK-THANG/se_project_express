a5e87ce0c49fcfc485e33b056ad5f0690891512f
Jmik Thang <jmikthang5@gmail.com>
2024-12-28 21:51:58 -0500

    test fail fix1

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
-const { getUsers, createUser, getUser } = require("../controllers/users");
+// external imports
 
 const router = require("express").Router();
+const { getUsers, createUser, getUser } = require("../controllers/users");
 router.get("/users", getUsers);
 router.get("/userId", getUser);
 router.post("/", createUser);

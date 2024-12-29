99b1e340bfbeb4951daf319d0983b9360fcf6cdc
Jmik Thang <jmikthang5@gmail.com>
2024-12-28 20:18:18 -0500

    getItem

--- app.js
+++ app.js
@@ -11,9 +11,10 @@ mongoose
   })
   .catch(console.error);
 
+const routes = require("./routes");
 app.use(express.json());
+app.use(routes);
 app.use("/", mainRouter);
-
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });

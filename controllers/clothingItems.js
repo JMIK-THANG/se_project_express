5e257413f5e542d1f07402cc9d0bd5f91b512bc3
Jmik Thang <jmikthang5@gmail.com>
2024-12-30 20:30:40 -0500

    validationError fix

--- controllers/clothingItems.js
+++ controllers/clothingItems.js
@@ -1,32 +1,34 @@
+
 const ClothingItem = require("../models/clothingItem");
 
 const createItem = (req, res) => {
   const { name, weather, imageUrl } = req.body;
 
-  ClothingItem.create({name, weather, imageUrl})
+  ClothingItem.create({ name, weather, imageUrl })
     .then((item) => {
       res.send(item);
     })
-    .catch((e) => {
-      if(e.name === "ValidatorError"){
-        return res.status(400).send({message: e.message});
+    .catch((err) => {
+      if (err.name === "ValidationError") {
+        res.status(400).send({ message: err.message });
+      } else {
+        res.status(500).send({ message: "Error from createItem", err });
       }
-      return res.status(500).send({ message: "Error from createItem", e });
     });
-  };
+};
 
-  const getItems = (req, res) => {
-    ClothingItem.find({})
+const getItems = (req, res) => {
+  ClothingItem.find({})
     .then((items) => res.status(200).send(items))
     .catch((e) => {
-      console.error(e)
+      console.error(e);
       res.status(500).send({ message: "Error from getItems", e });
     });
 };
 
 const updateItem = (req, res) => {
   const { itemId } = req.params;
-  const { imageUrl} = req.body;
+  const { imageUrl } = req.body;
   ClothingItem.findByIdAndUpdate(itemId, { $set: { imageUrl } })
     .orFail()
     .then((item) => res.status(200).send({ data: item }))
@@ -40,7 +42,7 @@ const deleteItem = (req, res) => {
   ClothingItem.findByIdAndDelete(itemId)
     .orFail()
     .then(() => res.status(204))
-    .send({ })
+    .send({})
     .catch((e) => {
       res.status(500).send({ message: "Error from deleteItem", e });
     });

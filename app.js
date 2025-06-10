dbf7bc52044c064396cd3b628f1f46bfbf6f5c00
Jmik Thang <jmikthang5@gmail.com>
2025-06-10 19:03:25 -0400

    env package install

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { errors } = require("celebrate");
 const mongoose = require("mongoose");
 const cors = require("cors");
+require("dotenv").config();
 const mainRouter = require("./routes/index");
 const errorHandler = require("./middlewares/error-handler");
 const { requestLogger, errorLogger } = require("./middlewares/logger");
